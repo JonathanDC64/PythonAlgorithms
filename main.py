a186e8006e8cfb68aa50934aa9c20ddd4f1dbea2
Jonathan Del Corpo <jonathan_delcorpo@hotmail.com>
2022-10-14 08:37:06 -0400

    Implementation of selection sort

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from algorithms.selection_sort import selection_sort
+
 def main() -> int:
-    print("Hello World")
+    selection_sort_input = [2,7,3,9,4,0,4]
+    print(f"Selection sort input {selection_sort_input}")
+
+    selection_sort(selection_sort_input)
+    print(f"Selection sort output {selection_sort_input}")
     return 0
 
 
